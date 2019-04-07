e44a62959ec76383aeb113c01726b06ce8f90754
1N0T <toni.solis.naranjo@gmail.com>
2019-04-07 21:13:49 +0200

    Añadidas tablas, relaciones y migrate

--- src/main.py
+++ src/main.py
@@ -1,15 +1,28 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy, event
+from flask_migrate import Migrate
 from config import DevConfig
+import datetime
 
 app = Flask(__name__)
 app.config.from_object(DevConfig)
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+# SQLite, por defecto no valida la existencia de claves externas, pero
+# podemos forzarlo con este procedimiento
+@event.listens_for(db.engine, "connect")
+def set_sqlite_pragma(dbapi_connection, connection_record):
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA foreign_keys=ON")
+    cursor.close()
+
+
 
 @app.route('/')
 def home():
     return '<h1>Hello World !!!</h1>'
-
 
 """
 Tipos se SQLAlchemy
@@ -21,34 +34,133 @@ db.Boolean
 db.Date
 db.DateTime
 db.Time
-
 Por defecto se asume que el nombre de la columna es igual al de la variable
-
     username = db.Column(db.String(255))
     username = db.Column('user_name', db.String(255))
-
 También se asume que la tabla tiene le mismo nombre que la clase, en el caso
 de que no sea así, se puede especicar el nombre de la misma.
-
     class User(db.Model):
         __tablename__ = 'user_table_name'
         ...
-
 Si no creamos el método __init__ SQLAlchemy creará uno por defecto, que espera
 recibir por parámetros los nombres y los valores de las columnas.
 """
 class User(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
-    username = db.Column(db.String(255))
+    username = db.Column(db.String(255), nullable=False, index=True, unique=True)
     password = db.Column(db.String(255))
+    posts =db.relationship(
+        'Post',
+        backref = 'user',
+        lazy = 'dynamic'
+    )
 
     def __init__(self, username):
-        self.username = usernme
+        self.username = username
 
     def __repr__(self):
         return "<User '{}'>".format(self.username)
 
 
+
+tags = db.Table (
+    "post_tags",
+    db.Column("post_id", db.Integer(), db.ForeignKey("post.id")),
+    db.Column("tag_id", db.Integer(), db.ForeignKey("tag.id"))
+)
+
+
+class Post(db.Model):
+    """ 
+    Notese que se ha especificado un string como parámetro de db.ForeignKey
+    ya que se podría dar la circuntancia de que no se hubiera creado todavía la 
+    clase User. El valor asignado tiene que ser coincidente con el especificado
+    en el parámetro backref de la relationship del objeto de clave externa.
+    """
+    id = db.Column(db.Integer(), primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    text = db.Column(db.Text())
+    publish_date = db.Column(db.DateTime(), default=datetime.datetime.now)
+    comments = db.relationship(
+        'Comment',
+        backref = 'post',
+        lazy = 'dynamic'
+    )
+    tags = db.relationship (
+        'Tag',
+        secondary = tags,
+        backref = db.backref('posts', lazy='dynamic')
+    )
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+
+    def __init__(self, title):
+        self.title = title
+
+    def __repr__(self):
+        return "<Post '{}'>".format(self.title)
+
+
+
+class Tag(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    title = db.Column(db.String(255), nullable=False, unique=True)
+
+    def __init__(self, title):
+        self.title = title
+
+    def __repr__(self):
+        return "<Tag '{}'>".format(self.title)
+
+
+
+class Comment(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    text = db.Column(db.Text())
+    date = db.Column(db.DateTime(), default=datetime.datetime.now)
+    post_id = db.Column(db.Integer, db.ForeignKey("post.id"))
+
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return "<Comment '{}'>".format(self.text[:15])
+
+
+
+"""
+user = User.query.get(1)
+new_post = Post('Post Title')
+new_post.user_id = user.id
+db.session.add(new_post)
+db.session.commit()
+
+o también podriamos hacer:
+
+user = User.query.get(1)
+new_post = Post('Post Title')
+user,posts.append(new_post)
+db.session.add(user)
+db.session.commit()
+
+Podemos tratar user.posts como una lista, o como una query
+user.posts.order_by(Post.publish_date.desc()).all()
+
+Creo 3 etiquetas y guardo 2 
+tag1 = Tag('tag 1')
+tag2 = Tag('tag 2')
+tag3 = Tag('tag 3')
+db.session.add(tag1)
+db.session.add(tag2)
+db.session.commit()
+
+Añado al no guardada a un post, al guardar éste, se guardará también el tag no guardado.
+post2 = Post.query.get(2)
+post2.tags.append(tag3)
+db.session.add(post2)
+db.session.commit()
+
+"""
+
 if __name__ == '__main__':
     app.run()
-
--- src/manage.py
+++ src/manage.py
@@ -1,4 +1,5 @@
-from main import app, db, User
+from main import app, db, User, Post, Comment, Tag, migrate
+from sqlalchemy.sql.expression import not_, or_
 
 """
 export FLASK_APP=manage.py
@@ -7,8 +8,81 @@ db.create_all()
 exit()
 
 sqlite3 database.db .tables
+
+user = User(username='Toni')
+db.session.add(user)
+db.session.commit()
+
+users = User.query.all()
+users
+    [<User 'Toni'>]
+
+users = User.query.limit(10).all()
+users = User.query.order_by(User.username).all()
+users = User.query.order_by(User.username.desc()).all()
+user = User.query.first()
+user.username 
+    'Toni'
+
+Acceso por primary key
+user = User.query.get(1)
+
+Paginaciones (primera de paginas de 10 elementos)
+paginas = User.query.paginate(1, 10)
+paginas.pages            (Número de páginas)
+paginas.page             (Página actual)
+paginas.has_prev         (True si existe página previa)
+paginas.has_next         (True si existe página siguiente)
+paginas.items            (lista de registros iterable con for)
+paginas = paginas.next() (Para obtener la página siguiente, si no existe permanece en la actual)
+paginas = paginas.prev() (Para obtener la página anterior, si no existe permanece en la actual)
+
+Filtros
+users = User.query.filter_by(username='Toni').all()
+users = User.query.order_by(User.username.desc()).filter_by(username='Toni').limit(2).all()
+user = User.query.filter(User.id > 1).all()
+
+
+from sqlalchemy.sql.expression import not_, or_
+user = User.query.filter(User.username.in_(['Toni']), User.password == None).first()
+user = User.query.filter(not_(User.password == None)).first()
+user = User.query.filter(or_(not_(User.password == None), User.id >= 1)).first()
+
+Actualizaciones
+User.query.filter_by(username='Toni').update({'password': 'test'})
+db.session.commit()
+
+Borrado
+user = User.query.filter_by(username='Paula').first()
+db.session.delete(user)
+db.session.commit()
+
+Para inicializar repositorio migrate
+export FLASK_APP=manage.py
+flask db init
+
+El siguiente comando guarda la estructura actual 
+flask db migrate -m "Versión inicial"
+
+El siguiente comando muestra los comandos DDL para generarlo
+flask db upgrade --sql
+
+Para actualizar la estructura
+flask db upgrade
+
+Para volver a una versión anterior
+flask db downgrade 7ded34bc4fb
+
 """
 
 @app.shell_context_processor
 def crear_contexto_shell():
-    return dict(app=app, db=db, User=User)
+    return dict(
+        app=app, 
+        db=db, 
+        User=User,
+        Comment=Comment,
+        Post=Post,
+        Tag=Tag,
+        migrate=migrate
+    )
